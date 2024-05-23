9eb0bb1a2220967e6ef87ddf7b0f6d1be3cd122c
Juan Ochoa <loljuanchogaymer@gmail.com>
2024-05-23 09:15:04 -0500

    Refresh de la pagina

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -18,6 +18,7 @@ class _HomePageState extends State<HomePage> {
     "images/7.jpeg",
     "images/8.jpeg",
   ];
+
   List<String> posts = [
     "images/post_1.jpeg",
     "images/post_2.jpeg",
@@ -28,6 +29,10 @@ class _HomePageState extends State<HomePage> {
     "images/post_7.jpeg",
     "images/post_8.jpeg",
   ];
+
+  Future<void> onRefresh()async{
+    await Future.delayed(const Duration(seconds: 1));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,107 +57,162 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            //STORY
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: List.generate(
-                  8,
-                  (index) => Container(
-                    padding: const EdgeInsets.all(10),
-                    child: Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 35,
-                          backgroundImage:
-                              const AssetImage("images/story.jpeg"),
-                          child: CircleAvatar(
-                            radius: 32,
-                            backgroundImage: AssetImage(
-                              profileImages[index],
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        const Text(
-                          "profile name",
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.black87,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            const Divider(),
-            Column(
-              children: List.generate(
-                8,
-                (index) => Column(
-                  children: [
-                    //HEADER
-                    Row(
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(10),
-                          child: CircleAvatar(
-                            radius: 14,
+      body: RefreshIndicator(
+        onRefresh: onRefresh,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              //STORY
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: List.generate(
+                    8,
+                    (index) => Container(
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            radius: 35,
                             backgroundImage:
                                 const AssetImage("images/story.jpeg"),
                             child: CircleAvatar(
-                              radius: 12,
+                              radius: 32,
                               backgroundImage: AssetImage(
                                 profileImages[index],
                               ),
                             ),
                           ),
-                        ),
-                        const Text('Profile Name'),
-                        // ignore: prefer_const_constructors
-                        Spacer(),
-                        IconButton(
-                          icon: const Icon(Icons.more_vert),
-                          onPressed: () {},
-                        )
-                      ],
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          const Text(
+                            "profile name",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black87,
+                            ),
+                          )
+                        ],
+                      ),
                     ),
-                    //IMAGE POST
-                    Image.asset(posts[index]),
-                    //FOOTER POST
-                    Row(
-                      children: [
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.favorite_border),
-                        ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.chat_bubble_outline),
-                        ),
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.label_outline),
-                        ),
-                        const Spacer(),
-                        IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.bookmark_add_outlined),
-                        ),
-                      ],
-                    )
-                  ],
+                  ),
                 ),
               ),
-            )
-          ],
+              const Divider(),
+              Column(
+                children: List.generate(
+                  8,
+                  (index) => Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //HEADER POST
+                      Row(
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(10),
+                            child: CircleAvatar(
+                              radius: 14,
+                              backgroundImage:
+                                  const AssetImage("images/story.jpeg"),
+                              child: CircleAvatar(
+                                radius: 12,
+                                backgroundImage: AssetImage(
+                                  profileImages[index],
+                                ),
+                              ),
+                            ),
+                          ),
+                          const Text('Profile Name'),
+                          // ignore: prefer_const_constructors
+                          Spacer(),
+                          IconButton(
+                            icon: const Icon(Icons.more_vert),
+                            onPressed: () {},
+                          )
+                        ],
+                      ),
+                      //IMAGE POST
+                      Image.asset(posts[index]),
+                      //FOOTER POST
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.favorite_border),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.chat_bubble_outline),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.label_outline),
+                          ),
+                          const Spacer(),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.bookmark_add_outlined),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        // ignore: prefer_const_constructors
+                        padding: EdgeInsets.all(15),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RichText(
+                              text: const TextSpan(
+                                style: TextStyle(color: Colors.black),
+                                children: [
+                                  TextSpan(text: "LIKE BY"),
+                                  TextSpan(
+                                    text: " Profile Name",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  TextSpan(text: " and"),
+                                  TextSpan(
+                                    text: " Others",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            RichText(
+                              text: const TextSpan(
+                                style: TextStyle(color: Colors.black),
+                                children: [
+                                  TextSpan(
+                                    text: " Profile Name",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text:
+                                        " This is the most azaming picture ever put on Instagram. This is also the best course ever made!",
+                                  ),
+                                ],
+                              ),
+                            ),
+                            const Text(
+                              'View all 12 comments',
+                              style: TextStyle(color: Colors.black38),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
